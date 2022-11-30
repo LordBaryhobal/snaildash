0108246838e807576b5417c01b478bd6cd6dde33
Lord Baryhobal <lordbaryhobal@gmail.com>
2022-11-30 19:00:09 +0100

    changed poison texture + reinforced drool textures

--- assets/textures/drool_poison.py
+++ assets/textures/drool_poison.py
@@ -11,9 +11,4 @@ for i in range(16):
 		r = random.randint(6,16)
 		pygame.draw.circle(S, (160,160,160), [x,y], r)
 	
-	for j in range(random.randint(6,12)):
-		x, y = random.randint(12,52), random.randint(12,52)
-		r = random.randint(3,8)
-		pygame.draw.circle(S, (0,0,0,0), [x,y], r)
-	
 	pygame.image.save(S, f"drool_poison/{i}.png")
